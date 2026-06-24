c9419fe07c89ba132c9b4e427f8d7408cab1c1a9
Luis <luis@staudt-fridingen.de>
2026-06-24 21:57:15 +0200

    updated route setup to fix ssh unreachable issue

--- WireGuard/install-wireguard-route.sh
+++ WireGuard/install-wireguard-route.sh
@@ -1,3 +1,12 @@
-sudo nano /etc/systemd/system/wireguard-route.service
-sudo systemctl enable wireguard-route.service
-sudo systemctl start wireguard-route.service
+cat > /etc/netplan/99-vpn-route.yaml <<'EOF'
+network:
+  version: 2
+  ethernets:
+    enp7s0:
+      routes:
+        - to: 10.2.0.0/24
+          via: 10.0.0.1
+EOF
+chmod 600 /etc/netplan/99-vpn-route.yaml
+netplan generate
+netplan apply
